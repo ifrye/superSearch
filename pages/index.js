1359c063194f4a92dad3846c944556a36873aab6
Ian Frye <ianfrye@Ians-MacBook-Pro.local>
2019-10-09 11:08:57 -0400

    All three pages are running. Only thing that is not done is the get request

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import React from "react";
-
-import {getInfo} from '../lib/utils.js';
+import Header from '../components/Header';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  
-
   render() {
     return (
      
-      <div style={{ margin: "auto auto", width: "1000px", textAlign: "center" }}>
+      <div style={{ margin: "auto auto", width: "1000px", textAlign: "center", border: "2px solid green" }}>
          <body>
-        <h1>Campgrounds</h1>
+            
+            <Header />
+            
+            <h1>Campgrounds</h1>
            
-        <img src="/static/westlake-campground.png" style={{height:400,width:500}} />
+            <img src="/static/westlake-campground.png" style={{height:400,width:500}} />
         
-        <p>Sometimes spending a day in the wilderness isn't quite enough to truly capture the feeling of a special place. 
-        Sure you see some amazing views or go on an unforgettable hike.  But to get the full experience, you yearn to see 
-        the dark, starry skies.  To listen to the Yips of coyotes. To watch the sun rise on a silent, dewy morning.  
-        Camping in the great outdoors transforms a regular old road trip into an epic experience. So pack up your tent or 
-        hitch up the fifth wheel -- adventure awaits!</p>
+            <p>Sometimes spending a day in the wilderness isn't quite enough to truly capture the feeling of a special place. 
+            Sure you see some amazing views or go on an unforgettable hike.  But to get the full experience, you yearn to see 
+            the dark, starry skies.  To listen to the Yips of coyotes. To watch the sun rise on a silent, dewy morning.  
+            Camping in the great outdoors transforms a regular old road trip into an epic experience. So pack up your tent or 
+            hitch up the fifth wheel -- adventure awaits!</p>
         
-        </body>
+          </body>
 
         <style jsx>{`
           h1,
@@ -33,49 +33,17 @@ class Home extends React.Component {
           p {
             font-family: "Arial";
             textAlign: center;
-          }
-          {
             color: green;
-          }
-
-          body{
-            border: 2px solid green;
-            padding 50px;
-          }
-
-          .container{
-            position: relative;
-            text-align: center;
-            color: white;
-          }
-
-          .center-text{
-            color: white;
-            text-align: center;
-            position: absolute;
           }
 
           .description {
             font-family: "Arial";
             font-size: "10px";
           }
-
-          ul {
-            padding: 0;
-          }
-
-          li {
-            list-style: none;
-            margin: 5px 0;
-          }
-          .App-logo {
-            height: 400px;
-            width 500px;
-          }
-
+          
           a {
-            text-decoration: none;
-            color: blue;
+            text-decoration: underline;
+            color: green;
           }
 
           a:hover {
